ba818241176766999948cd4241548ef100585da2
EstevesAndre <up201606673@fe.up.pt>
2018-11-06 12:28:48 +0000

    fixed bug, one of thousands

--- logic.pl
+++ logic.pl
@@ -5,25 +5,27 @@ checkGameState('P1', actualBoard).
 checkGameState('P2', actualBoard).
 
 /* Player 1 turn */
-p1Turn(board, newBoard, 'Player') :-
+p1Turn(Board, NewBoard, 'Player') :-
     nl, write('--------------- PLAYER1 ---------------'), nl.   
 
 /* Computer 1 turn */
-p1Turn(board, newBoard, 'Computer') :-    
+p1Turn(Board, NewBoard, 'Computer') :-    
     nl, write('-------------- COMPUTER1 --------------'), nl.
 
 /* Player 2 turn */
-p2Turn(board, newBoard, 'Player') :-
+p2Turn(Board, NewBoard, 'Player') :-
     nl, write('--------------- PLAYER2 ---------------'), nl.
 
 /* Computer 2 turn */
-p2Turn(board, newBoard, 'Computer') :-
+p2Turn(Board, NewBoard, 'Computer') :-
     nl, write('-------------- COMPUTER2 --------------'), nl.
 
 /* Loop do jogo */
 gameloop(Board, Player1, Player2) :-
+    write('asdsd'),
     p1Turn(Board, NewBoard, Player1),
     (
+        write('x'),
         (checkGameState('P1', NewBoard), write('\nNice Move!\n'));
         display_board(T,Player1),
         (p2Turn(NewBoard,NewNewBoard,Player2), 
@@ -39,4 +41,4 @@ gameloop(Board, Player1, Player2) :-
 startGame(Player1, Player2) :-
     initialBoard(T),
     display_board(T,Player1),
-    gameloop(T,Player1,Player2).
+    gameloop(T,Player1, Player2).
