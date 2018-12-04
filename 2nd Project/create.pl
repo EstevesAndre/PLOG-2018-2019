60b5883597f0322bc02160859fc2c049eb89449f
EstevesAndre <up201606673@fe.up.pt>
2018-12-04 09:35:02 +0000

    created draw, not completed

--- 2nd Project/create.pl
+++ 2nd Project/create.pl
@@ -3,7 +3,7 @@
 
 createSolution(Sol, N) :-
     NewN is N - 1,
-    length(Sol, NewN),
+    length(Sol, N),
     maplist(createLine(NewN), Sol).
 
 createLine(N, Elem) :-
@@ -18,3 +18,28 @@ createEmptySolution(Sol, N) :-
 createEmptyLine(N, Elem) :-
     length(Elem, N),
     maplist(random(0,1), Elem).
+
+createGrid(Lines,Columns, N):-
+    createSolution(Lines,N),
+    createSolution(Columns,N).
+
+drawSolution:-
+    createGrid(Lines,Columns, 4),
+    draw(Lines,Columns).
+
+draw(Lines,Columns):-
+    write(',---,---,---,---,'),nl,
+    drawGridSolution(Lines,Columns),
+    write('|---,---,---,---|'),nl.
+
+drawGridSolution([],[]).
+
+drawGridSolution([HeadL|Tail1],[HeadC|Tail2]):-
+    write('|'), drawLine(HeadL), nl,
+    write('|'), drawColumn(HeadC), nl,
+    drawGridSolution(Tail1,Tail2).
+
+% drawLine([]):- write('|').
+
+drawLine(_).
+drawColumn(_).
