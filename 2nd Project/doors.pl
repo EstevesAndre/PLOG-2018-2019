725bb1b120857d4b490809d8b25296b15fb162d2
luisdiogo98 <luisdiogo98@users.noreply.github.com>
2018-12-07 16:33:50 +0000

    Fixed display problem

--- 2nd Project/doors.pl
+++ 2nd Project/doors.pl
@@ -6,9 +6,9 @@
 doors(N) :-
     prompt(_, ''),
     nl, write('Puzzle:'), nl, nl,
-    createPuzzle(N, Puzzle, SolLines, SolCols),
-    % createEmptyGrid(EmptyLines, EmptyCols, N),
-    drawPuzzle(Puzzle, SolLines, SolCols, N),
+    createPuzzle(N, Puzzle, _, _),
+    createEmptyGrid(EmptyLines, EmptyCols, N),
+    drawPuzzle(Puzzle, EmptyLines, EmptyCols, N),
     write('Press ENTER to show solution.'),
     get_code(C),
     if_then_else(C =:= 10, true, skip_line),
@@ -19,7 +19,7 @@ doors(N) :-
     length(AuxCols, Length),
     append(AuxLines, AuxCols, Sol),
     AuxLength is N - 1,
-    listToMatrix(AuxLines, Lines, AuxLength, N),
-    listToMatrix(AuxCols, Cols, AuxLength, N),
+    listToMatrix(AuxLines, Lines, N, AuxLength),
+    listToMatrix(AuxCols, Cols, N, AuxLength),
     nl, write('Solution:'), nl, nl,
     drawPuzzle(Puzzle, Lines, Cols, N).
